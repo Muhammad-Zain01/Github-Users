f351c655c652881442ae6d7b3c971b5fc53250a5
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-11-01 16:21:41 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import SearchBox from './components/search-box';
+import GithubUsers from './components/github-users';
 function App() {
   return (
-    <div>
-      <SearchBox />
+    <div className='main-container'>
+      <div className='container'>
+        <SearchBox />
+        <GithubUsers />
+      </div>
     </div>
   );
 }
