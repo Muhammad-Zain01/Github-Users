c4633ac29a93603367c95dd4e4a642ff024c390c
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-11-03 17:01:05 +0500

    Updates

--- src/common/API.js
+++ src/common/API.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
-
 export const get = (url) => {
-    return axios.get(url)
+    let config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: url,
+        headers: {
+            'Authorization': 'Bearer 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'
+        }
+    };
+    return axios.request(config)
         .then(function (response) {
             return response;
         })
