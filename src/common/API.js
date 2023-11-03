2c5a61a86e4fb4d37df64473dcdf94dc66545294
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-11-03 17:11:08 +0500

    Update API.js

--- src/common/API.js
+++ src/common/API.js
@@ -4,9 +4,9 @@ export const get = (url) => {
         method: 'get',
         maxBodyLength: Infinity,
         url: url,
-        headers: {
-            'Authorization': 'Bearer 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'
-        }
+        // headers: {
+        //     'Authorization': 'Bearer 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'
+        // }
     };
     return axios.request(config)
         .then(function (response) {
