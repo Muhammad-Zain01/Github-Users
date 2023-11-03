c9c08d7d1ca25998b2a471fc846f275891aadb13
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-11-03 17:00:44 +0500

    Updates

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css';
+import SearchBox from './components/search-box';
+import GithubUsers from './components/github-users';
+import { get } from './common/API';
+import { useEffect, useState } from 'react';
+function App() {
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true);
+  const handleData = async (query = '') => {
+    let page = 1;
+    let per_page = 100;
+    setLoading(true);
+    let url = "https://api.github.com/users?&per_page=1000";
+    if(query !== '') url =  `https://api.github.com/search/users?q=${query}&per_page=${per_page}&page=${page}`;
+    const response = await get(url)
+    if(response.status == 200){
+      console.log(response.data);
+      setData(Array.isArray(response.data) ? response.data : response.data.items)
+      setLoading(false);
+    }
+  }
+  useEffect(() => {handleData()}, [])
+  return (
+    <div className='main-container'>
+      <div className='container' >
+        <SearchBox setQuery={(value) => handleData(value)}/>
+        <GithubUsers data={data} loading={loading}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
