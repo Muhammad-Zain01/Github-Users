eb6b1803e8ecbe19d4337df13c9e86a44ed6158f
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-11-06 12:43:41 +0500

    Update search-box.jsx

--- src/components/search-box.jsx
+++ src/components/search-box.jsx
@@ -3,12 +3,12 @@ import { Input, Space, Button } from 'antd'
 const SearchBox = ({ setQuery }) => {
     const [value, setValue] = useState("")
     return (
-        <>
-            <Space.Compact style={{ width: '100%' }}>
-                <Input placeholder="Search..." onKeyDown={(e) => e.key === 'Enter' && setQuery(value)} onChange={(e) => setValue(e.target.value)} />
-                <Button onClick={() => setQuery(value)} type="primary">Search</Button>
+        <div style={{display: 'flex', 'justifyContent':'center', marginBottom: 40, marginTop: 20}}>
+            <Space.Compact style={{ width: '50%' }}>
+                <Input size="large" placeholder="Search..." onKeyDown={(e) => e.key === 'Enter' && setQuery(value)} onChange={(e) => setValue(e.target.value)} />
+                <Button size="large" onClick={() => setQuery(value)} type="primary">Search</Button>
             </Space.Compact>
-        </>
+        </div>
     )
 }
 
