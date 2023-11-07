dcbae008ad3b81a21638ca3ac6762d9c099d7f32
zmemon <83939353+Muhammad-Zain01@users.noreply.github.com>
2023-11-07 10:37:05 +0500

    Update API.js

--- src/common/API.js
+++ src/common/API.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 export const get = (url) => {
+    const Auth = {
+        'Authorization': ''
+    }
+    const headers = Auth['Authorization'] != '' ? Auth : {}
     let config = {
         method: 'get',
         maxBodyLength: Infinity,
         url: url,
-        // headers: {
-        //     'Authorization': 'Bearer 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'
-        // }
+        headers: headers
     };
     return axios.request(config)
 }
